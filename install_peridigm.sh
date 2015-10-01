090ae9632fda50883c1265431cf27efca871b854
Ryan Murphy <murphy2902@gmail.com>
2015-10-01 03:14:48 -0400

    asdf

--- install_peridigm.sh
+++ install_peridigm.sh
@@ -66,50 +66,7 @@ if [[ ! -d ${T_DIR} ]] ; then
 	mkdir -v ${T_DIR}
 fi
 
-# Here we download all of the tar files we will need
-# The script checks to see if the tarfile already exists.  If it doesn't, it 
-# downloads and extracts it.  This makes it so that we don't have to repeatedly
-# download tarfiles if the script is run more than once.
-# TODO:  Version control for each of the dependencies
-[[ ! -e ${T_DIR}/boost.tar.gz ]] && \
-	wget -v -O ${T_DIR}/boost.tar.gz ${BOOST_URL} | tee ${REPORT_DIR}/boost_download_report.log && \
-	tar -xzf ${T_DIR}/boost.tar.gz -C ${BUILD_DIR} 
-[[ ! -e ${T_DIR}/hdf5.tar ]] && \
-	wget -v -O ${T_DIR}/hdf5.tar ${HDF5_URL} | tee ${REPORT_DIR}/hdf5_download_report.log && \
-	tar -xf ${T_DIR}/hdf5.tar -C ${BUILD_DIR} 
-[[ ! -e ${T_DIR}/netcdf.tar.gz ]] && \
-	wget -v -O ${T_DIR}/netcdf.tar.gz ${NETCDF_URL} | tee ${REPORT_DIR}/netcdf_download_report.log && \
-	tar -xzf ${T_DIR}/netcdf.tar.gz -C ${BUILD_DIR} 
-[[ ! -e ${T_DIR}/mpc.tar.gz ]] && \
-	wget -v -O ${T_DIR}/mpc.tar.gz ${MPC_URL} | tee ${REPORT_DIR}/mpc_download_report.log && \
-	tar -xzf ${T_DIR}/mpc.tar.gz -C ${BUILD_DIR} 
-[[ ! -e ${T_DIR}/mpfr.tar.gz ]] && \
-	wget -v -O ${T_DIR}/mpfr.tar.gz ${MPFR_URL} | tee ${REPORT_DIR}/mpfr_download_report.log && \
-	tar -xzf ${T_DIR}/mpfr.tar.gz -C ${BUILD_DIR} 
-[[ ! -e ${T_DIR}/gmp.tar.bz ]] && \
-	wget -v -O ${T_DIR}/gmp.tar.bz ${GMP_URL} | tee ${REPORT_DIR}/gmp_download_report.log && \
-	tar -xjf ${T_DIR}/gmp.tar.bz -C ${BUILD_DIR} 
-[[ ! -e ${T_DIR}/isl.tar.bz ]] && \
-	wget -v -O ${T_DIR}/isl.tar.bz ${ISL_URL} | tee ${REPORT_DIR}/isl_download_report.log && \
-	tar -xjf ${T_DIR}/isl.tar.bz -C ${BUILD_DIR}
-[[ ! -e ${T_DIR}/trilinos.tar.gz ]] && \
-	wget -v -O ${T_DIR}/trilinos.tar.gz ${TRILINOS_URL} | tee ${REPORT_DIR}/trilinos_download_report.log && \
-	tar -xzf ${T_DIR}/trilinos.tar.gz -C ${BUILD_DIR}
-
-# The locations that the source files were extracted to
-# In order to match and folder to the correct version, we do a search through
-# the directories only for the base library name.  We add this to the root 
-# directory to get an absolute path to the library.
-GMP=`ls ${BUILD_DIR} | grep -m 1 "gmp"`
-MPFR=`ls ${BUILD_DIR} | grep -m 1 "mpfr"`
-MPC=`ls ${BUILD_DIR} | grep -m 1 "mpc"`
-ISL=`ls ${BUILD_DIR} | grep -m 1 "isl"`
-BOOST=`ls ${BUILD_DIR} | grep -m 1 "boost"`
-HDF5=`ls ${BUILD_DIR} | grep -m 1 "hdf5"`
-NETCDF=`ls ${BUILD_DIR} | grep -m 1 "netcdf"`
-TRILINOS=`ls ${BUILD_DIR} | grep -m 1 "trilinos"`
-
-#if false; then
+if false; then
 # # # # # # # # # # # # # # # #
 # GMP
 # # # # # # # # # # # # # # # #
@@ -120,24 +77,27 @@ echo "---------- GMP    ----------"
 export CC=gcc
 export CXX=g++
 
-# For safety reasons, we always watch to attempt a fresh install of the
-# binaries.  This logic block will remove any previous install of the software.
-# TODO: Prompt to skip install if already installed.
-#if [[ -d ${BUILD_DIR}/$GMP-bin ]] ; then
-#	rm -rv ${BUILD_DIR}/$GMP-bin
-#fi
+[[ ! -e ${T_DIR}/gmp.tar.bz ]] && \
+	wget -v -O ${T_DIR}/gmp.tar.bz ${GMP_URL} | tee ${REPORT_DIR}/gmp_download_report.log && \
+	tar -xjf ${T_DIR}/gmp.tar.bz -C ${BUILD_DIR} 
 
-#mkdir -v ${BUILD_DIR}/$GMP-bin
-#cd ${BUILD_DIR}/$GMP-bin
+GMP=`ls ${BUILD_DIR} | grep -m 1 "gmp"`
 
-#${BUILD_DIR}/$GMP/configure \
-#--enable-static \
-#--with-pic \
-#--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/gmp_configure_report.log
+if [[ -d ${BUILD_DIR}/$GMP-bin ]] ; then
+	rm -rv ${BUILD_DIR}/$GMP-bin
+fi
 
-#make | tee ${REPORT_DIR}/gmp_make_report.log
-#make check | tee ${REPORT_DIR}/gmp_check_report.log
-#make install | tee ${REPORT_DIR}/gmp_install_report.log
+mkdir -v ${BUILD_DIR}/$GMP-bin
+cd ${BUILD_DIR}/$GMP-bin
+
+${BUILD_DIR}/$GMP/configure \
+--enable-static \
+--with-pic \
+--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/gmp_configure_report.log
+
+make | tee ${REPORT_DIR}/gmp_make_report.log
+make check | tee ${REPORT_DIR}/gmp_check_report.log
+make install | tee ${REPORT_DIR}/gmp_install_report.log
 
 
 # # # # # # # # # # # # # # # #
@@ -145,27 +105,29 @@ export CXX=g++
 # # # # # # # # # # # # # # # #
 
 
-#echo "---------- MPFR   ----------"
+echo "---------- MPFR   ----------"
 
+[[ ! -e ${T_DIR}/mpfr.tar.gz ]] && \
+	wget -v -O ${T_DIR}/mpfr.tar.gz ${MPFR_URL} | tee ${REPORT_DIR}/mpfr_download_report.log && \
+	tar -xzf ${T_DIR}/mpfr.tar.gz -C ${BUILD_DIR} 
 
-# For safety reasons, we always watch to attempt a fresh install of the
-# binaries.  This logic block will remove any previous install of the software.
-# TODO: Prompt to skip install if already installed.
-#if [[ -d ${BUILD_DIR}/${MPFR}-bin ]] ; then
-#	rm -r ${BUILD_DIR}/${MPFR}-bin
-#fi
+MPFR=`ls ${BUILD_DIR} | grep -m 1 "mpfr"`
 
-#mkdir -v ${BUILD_DIR}/${MPFR}-bin
-#cd ${BUILD_DIR}/${MPFR}-bin
+if [[ -d ${BUILD_DIR}/${MPFR}-bin ]] ; then
+	rm -r ${BUILD_DIR}/${MPFR}-bin
+fi
 
-#${BUILD_DIR}/${MPFR}/configure \
-#--enable-static \
-#--with-gmp=${ROOT_DIR} \
-#--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/mpfr_configure_report.log
+mkdir -v ${BUILD_DIR}/${MPFR}-bin
+cd ${BUILD_DIR}/${MPFR}-bin
 
-#make | tee ${REPORT_DIR}/mpfr_make_report.log
-#make check | tee ${REPORT_DIR}/mpfr_check_report.log
-#make install | tee ${REPORT_DIR}/mpfr_install_report.log
+${BUILD_DIR}/${MPFR}/configure \
+--enable-static \
+--with-gmp=${ROOT_DIR} \
+--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/mpfr_configure_report.log
+
+make | tee ${REPORT_DIR}/mpfr_make_report.log
+make check | tee ${REPORT_DIR}/mpfr_check_report.log
+make install | tee ${REPORT_DIR}/mpfr_install_report.log
 
 
 # # # # # # # # # # # # # # # #
@@ -175,22 +137,28 @@ export CXX=g++
 
 echo "---------- MPC    ----------"
 
-#if [[ -d ${BUILD_DIR}/${MPC}-bin ]] ; then
-	#rm -r ${BUILD_DIR}/${MPC}-bin
-#fi
+[[ ! -e ${T_DIR}/mpc.tar.gz ]] && \
+	wget -v -O ${T_DIR}/mpc.tar.gz ${MPC_URL} | tee ${REPORT_DIR}/mpc_download_report.log && \
+	tar -xzf ${T_DIR}/mpc.tar.gz -C ${BUILD_DIR} 
 
-#mkdir -v ${BUILD_DIR}/${MPC}-bin
-#cd ${BUILD_DIR}/${MPC}-bin
-#
-#${BUILD_DIR}/${MPC}/configure \
-#--prefix=${ROOT_DIR} \
-#--with-gmp=${ROOT_DIR} \
-#--with-mpfr=${ROOT_DIR} | tee ${REPORT_DIR}/mpc_configure_report.log
-#
-#make | tee ${REPORT_DIR}/mpc_make_report.log
-#make check | tee ${REPORT_DIR}/mpc_check_report.log
-#make install | tee ${REPORT_DIR}/mpc_install_report.log
-#
+MPC=`ls ${BUILD_DIR} | grep -m 1 "mpc"`
+
+if [[ -d ${BUILD_DIR}/${MPC}-bin ]] ; then
+	rm -r ${BUILD_DIR}/${MPC}-bin
+fi
+
+mkdir -v ${BUILD_DIR}/${MPC}-bin
+cd ${BUILD_DIR}/${MPC}-bin
+
+${BUILD_DIR}/${MPC}/configure \
+--prefix=${ROOT_DIR} \
+--with-gmp=${ROOT_DIR} \
+--with-mpfr=${ROOT_DIR} | tee ${REPORT_DIR}/mpc_configure_report.log
+
+make | tee ${REPORT_DIR}/mpc_make_report.log
+make check | tee ${REPORT_DIR}/mpc_check_report.log
+make install | tee ${REPORT_DIR}/mpc_install_report.log
+
 
 # # # # # # # # # # # # # # # #
 # ISL
@@ -199,22 +167,28 @@ echo "---------- MPC    ----------"
 
 echo "---------- ISL    ----------"
 
-#if test -d ${BUILD_DIR}/${ISL}-bin ; then
-	#rm -r ${BUILD_DIR}/${ISL}-bin
-#fi
-#
-#mkdir -v ${BUILD_DIR}/${ISL}-bin
-#cd ${BUILD_DIR}/${ISL}-bin
-#
-#${BUILD_DIR}/${ISL}/configure \
-#--enable-static \
-#--with-gmp-prefix=${ROOT_DIR} \
-#--without-piplib \
-#--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/isl_configure_report.log
-#
-#make | tee ${REPORT_DIR}/isl_make_report.log
-#make check | tee ${REPORT_DIR}/isl_check_report.log
-#make install | tee ${REPORT_DIR}/isl_install_report.log
+[[ ! -e ${T_DIR}/isl.tar.bz ]] && \
+	wget -v -O ${T_DIR}/isl.tar.bz ${ISL_URL} | tee ${REPORT_DIR}/isl_download_report.log && \
+	tar -xjf ${T_DIR}/isl.tar.bz -C ${BUILD_DIR}
+
+ISL=`ls ${BUILD_DIR} | grep -m 1 "isl"`
+
+if test -d ${BUILD_DIR}/${ISL}-bin ; then
+	rm -r ${BUILD_DIR}/${ISL}-bin
+fi
+
+mkdir -v ${BUILD_DIR}/${ISL}-bin
+cd ${BUILD_DIR}/${ISL}-bin
+
+${BUILD_DIR}/${ISL}/configure \
+--enable-static \
+--with-gmp-prefix=${ROOT_DIR} \
+--without-piplib \
+--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/isl_configure_report.log
+
+make | tee ${REPORT_DIR}/isl_make_report.log
+make check | tee ${REPORT_DIR}/isl_check_report.log
+make install | tee ${REPORT_DIR}/isl_install_report.log
 
 
 # # # # # # # # # # # # # # # #
@@ -225,33 +199,34 @@ echo "---------- ISL    ----------"
 echo "---------- GCC    ----------"
 
 
-#[[ ! -e ${T_DIR}/gcc.tar.gz ]] && \
-	#wget -v -O ${T_DIR}/gcc.tar.gz ${GCC_URL} | tee ${REPORT_DIR}/gcc_download_report.log && \
-	#tar -xzf ${T_DIR}/gcc.tar.gz -C ${BUILD_DIR} 
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${ROOT_DIR}/lib:${ROOT_DIR}/lib64"
+
+[[ ! -e ${T_DIR}/gcc.tar.gz ]] && \
+	wget -v -O ${T_DIR}/gcc.tar.gz ${GCC_URL} | tee ${REPORT_DIR}/gcc_download_report.log && \
+	tar -xzf ${T_DIR}/gcc.tar.gz -C ${BUILD_DIR} 
 
 GCC=`ls ${BUILD_DIR} | grep -m 1 "gcc"`
 
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${ROOT_DIR}/lib:${ROOT_DIR}/lib64"
-echo ${LD_LIBRARY_PATH}
+if test -d ${BUILD_DIR}/${GCC}-bin ; then
+	rm -r ${BUILD_DIR}/${GCC}-bin
+fi
 
-#if test -d ${BUILD_DIR}/${GCC}-bin ; then
-	#rm -r ${BUILD_DIR}/${GCC}-bin
-#fi
-#
-#mkdir -v ${BUILD_DIR}/${GCC}-bin
-#cd ${BUILD_DIR}/${GCC}-bin
-#
-##--with-gmp=${ROOT_DIR} \
-##--with-mpfr=${ROOT_DIR} \
-##--with-mpc=${ROOT_DIR} \
-##--with-isl=${ROOT_DIR} \
-#${BUILD_DIR}/${GCC}/configure \
-#--disable-multilib \
-#--enable-languages=c++,c,fortran \
-#--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/gcc_configure_report.log
-#make | tee ${REPORT_DIR}/gcc_make_report.log
-#make install | tee ${REPORT_DIR}/gcc_install_report.log
-#
+mkdir -v ${BUILD_DIR}/${GCC}-bin
+cd ${BUILD_DIR}/${GCC}-bin
+
+${BUILD_DIR}/${GCC}/contrib/download_prerequisites
+
+#--with-gmp=${ROOT_DIR} \
+#--with-mpfr=${ROOT_DIR} \
+#--with-mpc=${ROOT_DIR} \
+#--with-isl=${ROOT_DIR} \
+${BUILD_DIR}/${GCC}/configure \
+--disable-multilib \
+--enable-languages=c++,c,fortran \
+--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/gcc_configure_report.log
+make | tee ${REPORT_DIR}/gcc_make_report.log
+make install | tee ${REPORT_DIR}/gcc_install_report.log
+
 
 
 # # # # # # # # # # # # # # # #
@@ -265,25 +240,25 @@ export CC=${ROOT_DIR}/bin/gcc
 export CXX=${ROOT_DIR}/bin/g++
 export FC=${ROOT_DIR}/bin/gfortran
 
-#[[ ! -e ${T_DIR}/openmpi.tar.gz ]] && \
-	#wget -v -O ${T_DIR}/openmpi.tar.gz ${OPENMPI_URL} | tee ${REPORT_DIR}/openmpi_download_report.log && \
-	#tar -xzf ${T_DIR}/openmpi.tar.gz -C ${BUILD_DIR} 
+[[ ! -e ${T_DIR}/openmpi.tar.gz ]] && \
+	wget -v -O ${T_DIR}/openmpi.tar.gz ${OPENMPI_URL} | tee ${REPORT_DIR}/openmpi_download_report.log && \
+	tar -xzf ${T_DIR}/openmpi.tar.gz -C ${BUILD_DIR} 
 
 OPENMPI=`ls ${BUILD_DIR} | grep -m 1 "openmpi"`
 
-#if test -d ${BUILD_DIR}/${OPENMPI}-bin ; then
-	#rm -r ${BUILD_DIR}/${OPENMPI}-bin
-#fi
+if test -d ${BUILD_DIR}/${OPENMPI}-bin ; then
+	rm -r ${BUILD_DIR}/${OPENMPI}-bin
+fi
 
-#mkdir -v ${BUILD_DIR}/${OPENMPI}-bin
-#cd ${BUILD_DIR}/${OPENMPI}-bin
+mkdir -v ${BUILD_DIR}/${OPENMPI}-bin
+cd ${BUILD_DIR}/${OPENMPI}-bin
 
-#../${OPENMPI}/configure \
-#--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/openmpi_configure_report.log
+../${OPENMPI}/configure \
+--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/openmpi_configure_report.log
 
-#make all | tee ${REPORT_DIR}/openmpi_make_report.log
-#make check | tee ${REPORT_DIR}/openmpi_check_report.log
-#make install | tee ${REPORT_DIR}/openmpi_install_report.log
+make all | tee ${REPORT_DIR}/openmpi_make_report.log
+make check | tee ${REPORT_DIR}/openmpi_check_report.log
+make install | tee ${REPORT_DIR}/openmpi_install_report.log
 
 
 # # # # # # # # # # # # # # # #
@@ -298,20 +273,19 @@ export CXX=${ROOT_DIR}/bin/mpicxx
 export FC=${ROOT_DIR}/bin/mpif90
 export F77=${ROOT_DIR}/bin/mpif77
 
-if test ! -n ${BOOST} ; then
-   echo "uh oh"
-   exit 1
-fi
+[[ ! -e ${T_DIR}/boost.tar.gz ]] && \
+	wget -v -O ${T_DIR}/boost.tar.gz ${BOOST_URL} | tee ${REPORT_DIR}/boost_download_report.log && \
+	tar -xzf ${T_DIR}/boost.tar.gz -C ${BUILD_DIR} 
+
+BOOST=`ls ${BUILD_DIR} | grep -m 1 "boost"`
 
 cd ${BUILD_DIR}/${BOOST}
 
 # Configure boost
-#./bootstrap.sh | tee ${REPORT_DIR}/boost_configure_report.log
+./bootstrap.sh | tee ${REPORT_DIR}/boost_configure_report.log
 
-#echo asdf
-# Check to see if boost is already installed
-#./b2 install \
-#--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/boost_install_report.log
+./b2 install \
+--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/boost_install_report.log
 
 
 # # # # # # # # # # # # # # # #
@@ -319,7 +293,19 @@ cd ${BUILD_DIR}/${BOOST}
 # # # # # # # # # # # # # # # #
 
 
+fi
 echo "---------- HDF5    ----------"
+
+export FC=${ROOT_DIR}/bin/mpif90
+export F77=${ROOT_DIR}/bin/mpif77
+export CC=${ROOT_DIR}/bin/mpicc
+export CXX=${ROOT_DIR}/bin/mpic++
+
+[[ ! -e ${T_DIR}/hdf5.tar ]] && \
+	wget -v -O ${T_DIR}/hdf5.tar ${HDF5_URL} | tee ${REPORT_DIR}/hdf5_download_report.log && \
+	tar -xf ${T_DIR}/hdf5.tar -C ${BUILD_DIR} 
+
+HDF5=`ls ${BUILD_DIR} | grep -m 1 "hdf5"`
 
 if [[ -d ${BUILD_DIR}/${HDF5}-bin ]] ; then
 	rm -r ${BUILD_DIR}/${HDF5}-bin
@@ -328,14 +314,14 @@ fi
 mkdir -v ${BUILD_DIR}/${HDF5}-bin
 cd ${BUILD_DIR}/${HDF5}-bin
 
-#../${HDF5}/configure \
-#--enable-parallel \
-#--enable-static \
-#--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/hdf5_configure_report.log
+../${HDF5}/configure \
+--enable-parallel \
+--enable-static \
+--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/hdf5_configure_report.log
 
-#make | tee ${REPORT_DIR}/hdf5_make_report.log 
-#make check | tee ${REPORT_DIR}/hdf_check_report.log
-#make install | tee ${REPORT_DIR}/hdf5_install_report.log
+make | tee ${REPORT_DIR}/hdf5_make_report.log 
+make check | tee ${REPORT_DIR}/hdf_check_report.log
+make install | tee ${REPORT_DIR}/hdf5_install_report.log
 
 
 # # # # # # # # # # # # # # # #
@@ -349,10 +335,12 @@ export CPPFLAGS="${CPPFLAGS} -I${ROOT_DIR}/include"
 export CXXFLAGS=" -std=c++11 -Wall -g"
 export LDFLAGS="${LDFLAGS} -L${ROOT_DIR}/lib"
 
+[[ ! -e ${T_DIR}/netcdf.tar.gz ]] && \
+	wget -v -O ${T_DIR}/netcdf.tar.gz ${NETCDF_URL} | tee ${REPORT_DIR}/netcdf_download_report.log && \
+	tar -xzf ${T_DIR}/netcdf.tar.gz -C ${BUILD_DIR} 
 
-# For safety reasons, we always watch to attempt a fresh install of the
-# binaries.  This logic block will remove any previous install of the software.
-# TODO: Prompt to skip install if already installed.
+NETCDF=`ls ${BUILD_DIR} | grep -m 1 "netcdf"`
+
 if test -d ${BUILD_DIR}/${NETCDF}-bin ; then
 	rm -r ${BUILD_DIR}/${NETCDF}-bin
 fi
@@ -364,15 +352,17 @@ echo "Don't forget to edit the files!"
 # TODO: Edit files with script
 
 
-#../${NETCDF}/configure \
-#--disable-netcdf-4 \
-##--disable-dap \
-#--enable-static \
-#--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/netcdf_configure_report.log
+../${NETCDF}/configure \
+--disable-netcdf-4 \
+#--disable-dap \
+--enable-static \
+--prefix=${ROOT_DIR} | tee ${REPORT_DIR}/netcdf_configure_report.log
 
-#make | tee ${REPORT_DIR}/netcdf_make_report.log
-#make check | tee ${REPORT_DIR}/netcdf_check_report.log
-#make install | tee ${REPORT_DIR}/netcdf_install_report.log
+make | tee ${REPORT_DIR}/netcdf_make_report.log
+make check | tee ${REPORT_DIR}/netcdf_check_report.log
+make install | tee ${REPORT_DIR}/netcdf_install_report.log
+
+cp -vf ${BUILD_DIR}/${NETCDF}/include/netcdf_par.h ${ROOT_DIR}/include/
 
 
 # # # # # # # # # # # # # # # #
@@ -389,13 +379,12 @@ fi
 mkdir -vp ${BUILD_DIR}/${MATIO}-bin
 cd ${BUILD_DIR}/${MATIO}-bin
 
-#../${MATIO}/configure \
-#--prefix=${ROOT_DIR}
-#
-#make
-#make check
-#make install
+../${MATIO}/configure \
+--prefix=${ROOT_DIR}
 
+make
+make check
+make install
 
 
 # # # # # # # # # # # # # # # #
@@ -403,17 +392,19 @@ cd ${BUILD_DIR}/${MATIO}-bin
 # # # # # # # # # # # # # # # #
 
 
-# For safety reasons, we always watch to attempt a fresh install of the
-# binaries.  This logic block will remove any previous install of the software.
-# TODO: Prompt to skip install if already installed.
-
 export CC=${ROOT_DIR}/${OPENMPI}/bin/mpicc
 export CXX=${ROOT_DIR}/${OPENMPI}/bin/mpicxx
 export FC=${ROOT_DIR}/${OPENMPI}/bin/mpif90
 export F77=${ROOT_DIR}/${OPENMPI}/bin/mpif77
 export CPPFLAGS="${CPPFLAGS} -I${ROOT_DIR}/include"
 export CXXFLAGS=" -std=gnu++0x -Wall -g"
-export LDFLAGS="${LDFLAGS} -L${ROOT_DIR}/lib"
+export LDFLAGS="${LDFLAGS} -L${ROOT_DIR}/lib -Ldl"
+
+[[ ! -e ${T_DIR}/trilinos.tar.gz ]] && \
+	wget -v -O ${T_DIR}/trilinos.tar.gz ${TRILINOS_URL} | tee ${REPORT_DIR}/trilinos_download_report.log && \
+	tar -xzf ${T_DIR}/trilinos.tar.gz -C ${BUILD_DIR}
+
+TRILINOS=`ls ${BUILD_DIR} | grep -m 1 "trilinos"`
 
 if test -d ${BUILD_DIR}/${TRILINOS}-bin ; then
    rm  -vr ${BUILD_DIR}/${TRILINOS}-bin
@@ -480,4 +471,45 @@ ${BUILD_DIR}/${TRILINOS} \
 make | tee ${REPORT_DIR}/trilinos_make_report.log
 make check | tee ${REPORT_DIR}/trilinos_check_report.log
 make install | tee ${REPORT_DIR}/trilinos_install_report.log
-echo "Trilinos Installed!"
+
+
+# # # # # # # # # # # # # # # #
+#  Peridigm
+# # # # # # # # # # # # # # # #
+
+
+export CC=${ROOT_DIR}/${OPENMPI}/bin/mpicc
+export CXX=${ROOT_DIR}/${OPENMPI}/bin/mpicxx
+export FC=${ROOT_DIR}/${OPENMPI}/bin/mpif90
+export F77=${ROOT_DIR}/${OPENMPI}/bin/mpif77
+export CPPFLAGS="-I${ROOT_DIR}/include"
+export CXXFLAGS="-std=gnu++0x -Wall -g"
+export LDFLAGS="-L${ROOT_DIR}/lib -Ldl"
+export PATH=${PATH}:${ROOT_DIR}/bin
+
+PERIDIGM=`ls ${BUILD_DIR} | grep -m 1 "Peridigm"`
+
+if test -d ${BUILD_DIR}/${PERIDIGM}-bin ; then
+   rm -r ${BUILD_DIR}/${PERIDIGM}-bin
+fi
+
+mkdir -vp ${BUILD_DIR}/${PERIDIGM}-bin
+cd ${BUILD_DIR}/${PERIDIGM}-bin
+
+cmake \
+-D CMAKE_BUILD_TYPE:STRING=Release \
+-D Trilinos_DIR:PATH=${ROOT_DIR}/lib/cmake/Trilinos/ \
+-D CMAKE_C_COMPILER:STRING=${ROOT_DIR}/bin/mpicc \
+-D CMAKE_CXX_COMPILER:STRING=${ROOT_DIR}/bin/mpicxx \
+-D BOOST_ROOT=${ROOT_DIR} \
+-D Boost_INCLUDE_DIRS:PATH=${ROOT_DIR}/include \
+-D Boost_LIBRARY_DIRS:PATH=${ROOT_DIR}/lib \
+-D HDF5_LIBRARY_DIRS:PATH="${ROOT_DIR}/lib" \
+-D HDF5_INCLUDE_DIRS:PATH="${ROOT_DIR}/include" \
+-D Netcdf_INCLUDE_DIRS:PATH=${ROOT_DIR}/include \
+-D Netcdf_LIBRARY_DIRS:PATH=${ROOT_DIR}/lib \
+-D CMAKE_CXX_FLAGS:STRING="-O2 -Wall -ansi -pedantic -Wno-long-long -ftrapv -Wno-depracated -std=gnu++0x" \
+${BUILD_DIR}/${PERIDIGM}
+
+make | tee ${REPORT_DIR}/peridigm_make_report.log
+make test | tee ${REPORT_DIR}/peridigm_check_report.log
